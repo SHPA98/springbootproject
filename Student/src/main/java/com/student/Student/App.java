8c229f6d4321c02198647d6045f48e8a2d211b2c
Pawan Sharma <Ksharma.pawan98@gmail.com>
2022-02-10 10:12:33 +0530

    Student

---
+++ Student/src/main/java/com/student/Student/App.java
@@ -0,0 +1,13 @@
+package com.student.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
